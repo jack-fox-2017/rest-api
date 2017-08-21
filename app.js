b8dc5696e0a91ea989f5b4c490634e5652d98668
pdvega <pdvegaa@gmail.com>
2017-08-21 11:27:52 +0700

    installing express

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    mongoose = require('mongoose'),
+    cors = require('cors');
+    
+require('dotenv').config()
+    
+const index = require('./routes/index');
+const signup = require('./routes/signup');
+
+// app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+
+app.use('/', index);
+app.use('/signup', signup);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+app.listen(process.env.PORT || 3000);
