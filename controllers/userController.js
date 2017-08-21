47ea330b458cec84394e68f663d3b4f6be13ef15
Bambang Handoko <bambank.handoko@yahoo.com>
2017-08-21 20:13:19 +0700

    refactor: use middleware to authorize(admin or not)

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,36 +7,36 @@ require('dotenv').config()
 
 module.exports = {
 	getAll: (req, res) => {
-		if (req.headers.hasOwnProperty('token'))
-			jwt.verify(req.headers.token, process.env.TOKEN_SECRET_KEY, (err, decoded) => {
-				if (err == null)
-					if (decoded.role == 'admin')
+		// if (req.headers.hasOwnProperty('token'))
+			// jwt.verify(req.headers.token, process.env.TOKEN_SECRET_KEY, (err, decoded) => {
+				// if (err == null)
+					// if (decoded.role == 'admin')
 						db.User.findAll()
 						.then(result => {res.json(result)})
 						.catch(err => {res.json(err)})
-					else res.send('bukan admin')
-				else res.send(err)
-			})
-		else res.send('belom login')
+					// else res.send('bukan admin')
+				// else res.send(err)
+			// })
+		// else res.send('belom login')
 	},
 
 	getById: (req, res) => {
-		if (req.headers.hasOwnProperty('token'))
-			jwt.verify(req.headers.token, process.env.TOKEN_SECRET_KEY, (err, decoded) => {
-				if (err == null)
+		// if (req.headers.hasOwnProperty('token'))
+			// jwt.verify(req.headers.token, process.env.TOKEN_SECRET_KEY, (err, decoded) => {
+				// if (err == null)
 					db.User.findById(req.params.id)
 					.then(result => {res.json(result)})
 					.catch(err => {res.json(err)})
-				else res.send(err)
-			})
-		else res.send('belom login')
+				// else res.send(err)
+			// })
+		// else res.send('belom login')
 	},
 
 	create: (req, res) => {
-		if (req.headers.hasOwnProperty('token'))
-			jwt.verify(req.headers.token, process.env.TOKEN_SECRET_KEY, (err, decoded) => {
-				if (err == null)
-					if (decoded.role == 'admin') {
+		// if (req.headers.hasOwnProperty('token'))
+			// jwt.verify(req.headers.token, process.env.TOKEN_SECRET_KEY, (err, decoded) => {
+				// if (err == null) {
+					// if (decoded.role == 'admin') {
 						let createFunc = () => {
 							req.body.salt = randomSalt(8)
 							db.User.create(req.body)
@@ -49,36 +49,36 @@ module.exports = {
 							})
 						}
 						createFunc()
-					} else res.send('bukan admin')
-				else res.send(err)
-			})
-		else res.send('belom login')
+					// } else res.send('bukan admin')
+				// } else res.send(err)
+			// })
+		// else res.send('belom login')
 	},
 
 	destroyById: (req, res) => {
-		if (req.headers.hasOwnProperty('token'))
-			jwt.verify(req.headers.token, process.env.TOKEN_SECRET_KEY, (err, decoded) => {
-				if (err == null)
-					if (decoded.role == 'admin')
+		// if (req.headers.hasOwnProperty('token'))
+			// jwt.verify(req.headers.token, process.env.TOKEN_SECRET_KEY, (err, decoded) => {
+				// if (err == null)
+					// if (decoded.role == 'admin')
 						db.User.destroy({where: {id: req.params.id}})
 						.then(result => {res.json(result)})
 						.catch(err => {res.json(err)})
-					else res.send('bukan admin')
-				else res.send(err)
-			})
-		else res.send('belom login')
+					// else res.send('bukan admin')
+				// else res.send(err)
+			// })
+		// else res.send('belom login')
 	},
 
 	updateById: (req, res) => {
-		if (req.headers.hasOwnProperty('token'))
-			jwt.verify(req.headers.token, process.env.TOKEN_SECRET_KEY, (err, decoded) => {
-				if (err == null)
+		// if (req.headers.hasOwnProperty('token'))
+			// jwt.verify(req.headers.token, process.env.TOKEN_SECRET_KEY, (err, decoded) => {
+				// if (err == null)
 					db.User.update(req.body, {where: {id: req.params.id}, individualHooks: true})
 					.then(result => {res.json(result)})
 					.catch(err => {res.json(err)})
-				else res.send(err)
-			})
-		else res.send('belom login')
+				// else res.send(err)
+			// })
+		// else res.send('belom login')
 	},
 
 	signIn: (req, res) => {
--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,33 @@
 var express = require('express');
 var router = express.Router();
 var userController = require('../controllers/userController')
+const jwt = require('jsonwebtoken')
 
 /* GET users listing. */
 
 router.post('/signin', userController.signIn)
-
 router.post('/signup', userController.signUp)
 
-router.get('/users/', userController.getAll)
+router.use((req, res, next) => {
+  if (req.headers.hasOwnProperty('token')) next()
+  else res.send('belom login')
+})
 
 router.get('/users/:id', userController.getById)
+router.put('/users/:id', userController.updateById)
 
+router.use((req, res, next) => {
+  jwt.verify(req.headers.token, process.env.TOKEN_SECRET_KEY, (err, decoded) => {
+    if (err == null)
+      if (decoded.role == 'admin') next()
+      else res.send('bukan admin')
+    else res.send(err)
+  })
+})
+
+router.get('/users/', userController.getAll)
 router.post('/users/', userController.create)
-
 router.delete('/users/:id', userController.destroyById)
 
-router.put('/users/:id', userController.updateById)
 
 module.exports = router;
