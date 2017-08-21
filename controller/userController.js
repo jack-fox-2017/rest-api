61d13f7595528724f3ca5fb5426a6393522e28ba
rahid <hidayat.85@gmail.com>
2017-08-21 20:47:36 +0700

    authentication token user with sign in

--- controller/userController.js
+++ controller/userController.js
@@ -1,21 +1,117 @@
 const model = require ('../models')
+const jwt = require('jsonwebtoken');
+const secret = require ('../helpers/secretKey')
+const crypPass = require ('../helpers/encrypt')
+require('dotenv').config()
 
 const getAllUsers = function (req, res) {
-  model.User.findAll()
-  .then(data => res.json(data))
-  .catch(err => res.send(err))
+  let token = req.headers.token
+  let decode = jwt.verify(token, process.env.USER_TOKEN)
+  if(decode.role == 'admin') {
+    model.User.findAll()
+    .then(data => res.send(data))
+    .catch(err => res.send(err))
+  } else {
+    res.send(`you don't have access`)
+  }
 }
 
 const getUserById = (req,res) => {
-  model.User.findById(req.params.id)
-  .then(data => res.send(data))
-  .catch(err => res.send(err))
+  let token = req.headers.token
+  let decode = jwt.verify(token, process.env.USER_TOKEN)
+  if (decode.role == 'admin' || decode.id == req.params.id) {
+    model.User.findById(req.params.id)
+    .then(data => res.send(data))
+    .catch(err => res.send(err))
+  }
+  else {
+    res.send(`you don't have access`)
+  }
 }
 
 const createUser = (req,res) => {
+  let token = req.headers.token
+  let decode = jwt.verify(token, process.env.USER_TOKEN)
+  if(decode.role == 'admin') {
+    model.User.create({
+      username: req.body.username,
+      password: req.body.password,
+      role: req.body.role,
+      key: secret(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
+    .then(() => res.send ('data created'))
+    .catch(err => res.send(err))
+  } else {
+    res.send(`you don't have authorize`)
+  }
+}
+
+const deleteUser = (req,res) => {
+  let token = req.headers.token
+  let decode = jwt.verify(token, process.env.USER_TOKEN)
+  if(decode.role == 'admin') {
+    model.User.destroy({
+      where: {id: req.params.id}
+    })
+    .then(() => res.send('deleted success'))
+    .catch(err => res.send(err))
+  } else {
+    res.send(`you don't have authorize`)
+  }
+}
+
+const updateUser = (req,res) => {
+  let token = req.headers.token
+  let decode = jwt.verify(token, process.env.USER_TOKEN)
+  if (decode.role == 'admin' || decode.id == req.params.id) {
+    model.User.update({
+      username: req.body.username,
+      password: req.body.password,
+      role: req.body.role,
+      key: secret(),
+      updatedAt: new Date()
+    }, {
+      where: {id: req.params.id},
+      individualHooks: true
+    })
+    .then(() => res.send ('updated success'))
+    .catch(err => res.send(err))
+  } else {
+    res.send(`you don't have authorize`)
+  }
+
+}
+
+const signinUser = (req,res) => {
+  model.User.findOne({
+    where: {username: req.body.username}
+  })
+  .then(data => {
+    if(data.password == crypPass(req.body.password, data.key)) {
+
+      let token = jwt.sign({
+        id: data.id,
+        username: data.username,
+        role: data.role
+      }, process.env.USER_TOKEN)
+
+      res.send(token)
+
+    } else {
+      res.send('wrong password')
+    }
+  })
+  .catch(err => res.send(err))
+}
+
+const signupUser = (req,res) => {
   model.User.create({
     username: req.body.username,
     password: req.body.password,
+    role: req.body.role,
+    key: secret(),
     createdAt: new Date(),
     updatedAt: new Date()
   })
@@ -23,26 +119,12 @@ const createUser = (req,res) => {
   .catch(err => res.send(err))
 }
 
-const deleteUser = (req,res) => {
-  model.User.destroy({
-    where: {id: req.params.id}
-  })
-  .then(() => res.send('delete sukses'))
-  .catch(err => res.send(err))
-}
-
-const updateUser = (req,res) => {
-  model.User.update(req.body, {
-    where: {id: req.params.id}
-  })
-  .then(() => res.send ('update sukses'))
-  .catch(err => res.send(err))
-}
-
 module.exports = {
   getAllUsers,
   getUserById,
   createUser,
+  signupUser,
+  signinUser,
   deleteUser,
   updateUser
 }
--- models/user.js
+++ models/user.js
@@ -1,12 +1,22 @@
 'use strict';
+// var secret = require ('../helpers/secretKey')
+const crypPass = require ('../helpers/encrypt')
+
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     username: DataTypes.STRING,
-    password: DataTypes.STRING
+    password: DataTypes.STRING,
+    role: DataTypes.STRING,
+    key: DataTypes.STRING
   }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
+    hooks: {
+      beforeCreate: (models) => {
+        let tempPass = models.password;
+        models.password = crypPass (tempPass, models.key);
+      },
+      beforeUpdate: (models) => {
+        let tempPass = models.password;
+        models.password = crypPass (tempPass, models.key);
       }
     }
   });
